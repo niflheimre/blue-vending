fa7d590b7d696cd0ee8885c0cc2e819e602a8052
NaksomboonK <89895642+NaksomboonK@users.noreply.github.com>
2023-06-13 23:58:07 +0700

    fix: add pre build script and insert query string

---
+++ pre-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd postgres
+mkdir pg_commit_ts
+mkdir pg_dynshmem
+mkdir pg_logical/mappings
+mkdir pg_logical/snapshots
+mkdir pg_notify
+mkdir pg_replslot
+mkdir pg_serial
+mkdir pg_snapshots
+mkdir pg_stat
+mkdir pg_stat_tmp
+mkdir pg_tblspc
+mkdir pg_twophase
+mkdir pg_wal/archive_status
+
+
